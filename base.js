ea228913acd3c2533811e2d40d6f2cf3fa3824ab
Marcelo Rodriguez <marcelo.rodriguez@jam3.com>
2017-06-26 20:51:52 -0300

    Tweens and Promises library have to be passed as arguments

--- base.js
+++ base.js
@@ -1,74 +1,80 @@
-var TweenMax = require('gsap')
-var assign = require('object-assign')
+var assign = require('object-assign');
 
-module.exports = function(Promise) {
+module.exports = function(Tweens, Promise) {
+  Promise.config({
+    cancellation: true
+  });
 
-	Promise.config({
-		cancellation: true
-	})
+  function animateFunc(func, element, duration, opts) {
+    opts = assign({}, opts);
+    var tween;
+    return new Promise(function(resolve, reject, onCancel) {
+      opts.onComplete = resolve;
+      tween = func(element, duration, opts);
+      onCancel &&
+        onCancel(function() {
+          tween.kill();
+        });
+    });
+  }
 
-	function animateFunc(func, element, duration, opts) {
-		opts = assign({}, opts)
-		var tween
-		return new Promise(function(resolve, reject, onCancel) {
-			opts.onComplete = resolve
-			tween = func(element, duration, opts)
-			onCancel && onCancel(function(){
-				tween.kill()
-			})
-		})
-	}
+  var animateTo = animateFunc.bind(null, Tweens.to);
 
-	var animateTo = animateFunc.bind(null, TweenMax.to)
+  var util = animateTo;
+  util.to = animateTo;
+  util.from = animateFunc.bind(null, Tweens.from);
 
-	var util = animateTo
-	util.to = animateTo
-	util.from = animateFunc.bind(null, TweenMax.from)
+  util.set = function animateSet(element, params) {
+    params = assign({}, params);
+    return new Promise(function(resolve, reject) {
+      params.onComplete = resolve;
+      Tweens.set(element, params);
+    });
+  };
 
-	util.set = function animateSet(element, params) {
-		params = assign({}, params)
-		return new Promise(function(resolve, reject) {
-			params.onComplete = resolve
-			TweenMax.set(element, params)
-		})
-	}
+  util.fromTo = function animateFromTo(element, duration, from, to) {
+    to = assign({}, to);
+    var tween;
+    return new Promise(function(resolve, reject, onCancel) {
+      to.onComplete = resolve;
+      tween = Tweens.fromTo(element, duration, from, to);
+      onCancel &&
+        onCancel(function() {
+          tween.kill();
+        });
+    });
+  };
+  ['staggerTo', 'staggerFrom'].forEach(function(fn) {
+    var tweenFunc = Tweens[fn];
+    var tweens;
+    util[fn] = function(element, duration, from, stagger) {
+      return new Promise(function(resolve, reject, onCancel) {
+        tweens = tweenFunc(element, duration, from, stagger, resolve);
+        onCancel &&
+          onCancel(function() {
+            tweens.forEach(function(tween) {
+              tween.kill();
+            });
+          });
+      });
+    };
+  });
 
-	util.fromTo = function animateFromTo(element, duration, from, to) {
-		to = assign({}, to)
-		var tween
-		return new Promise(function(resolve, reject, onCancel) {
-			to.onComplete = resolve
-			tween = TweenMax.fromTo(element, duration, from, to)
-			onCancel && onCancel(function(){
-				tween.kill()
-			})
-		})
-	}
+  util.staggerFromTo = function staggerFromTo(element, duration, from, to, stagger, position) {
+    var tweens;
+    return new Promise(function(resolve, reject, onCancel) {
+      tweens = Tweens.staggerFromTo(element, duration, from, to, stagger, resolve);
+      onCancel &&
+        onCancel(function() {
+          tweens.forEach(function(tween) {
+            tween.kill();
+          });
+        });
+    });
+  };
 
-	;['staggerTo', 'staggerFrom'].forEach(function(fn) {
-		var tweenFunc = TweenMax[fn]
-		var tweens
-		util[fn] = function(element, duration, from, stagger) {
-			return new Promise(function(resolve, reject, onCancel) {
-				tweens = tweenFunc(element, duration, from, stagger, resolve)
-				onCancel && onCancel(function(){
-					tweens.forEach( function (tween) { tween.kill() })
-				})
-			})
-		}
-	})
+  util.killTweensOf = Tweens.killTweensOf;
 
-	util.staggerFromTo = function staggerFromTo(element, duration, from, to, stagger, position) {
-		var tweens
-		return new Promise(function(resolve, reject, onCancel) {
-			tweens = TweenMax.staggerFromTo(element, duration, from, to, stagger, resolve)
-			onCancel && onCancel(function(){
-				tweens.forEach( function (tween) { tween.kill() })
-			})
-		})
-	}
-
-
-	util.all = Promise.all
-	return util
-}
+  util.all = Promise.all;
+  return util;
+};
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = require('./base')(require('bluebird'))
+module.exports = function(Tweens, Promise) {
+  !Tweens && console.error('Tween library missing as first argument!');
+  !Promise && console.error('Promise library missing as second argument!');
+  require('./base')(Tweens, Promise);
+};
